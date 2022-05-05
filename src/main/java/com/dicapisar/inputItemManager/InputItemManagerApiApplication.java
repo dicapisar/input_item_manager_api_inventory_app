a95539f43421ae75440e12df87fcbbd6c3e95fe1
dicapisar <dicapisar@gmail.com>
2022-05-04 22:38:30 -0500

    Initialitation Proyect

---
+++ src/main/java/com/dicapisar/inputItemManager/InputItemManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.inputItemManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputItemManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputItemManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/inputItemManager/InputItemManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.inputItemManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputItemManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
